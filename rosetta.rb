e812a02b91874b880b426ecbe84eaae745ee39f5
Andrew Hay <andrewsmhay@gmail.com>
2013-11-12 22:10:30 -0800

    vars

--- rosetta.rb
+++ rosetta.rb
@@ -225,8 +225,8 @@ elsif os_decided == "nix" && File.exist?(Variables.package_rh)
 	else ARGV[1] == Variables.opt_sel[2]
 		puts Messages.post_a_compare
 		Variables.name_files.each do |naming|
-			f1 = IO.readlines(workingdir + "/" + naming + ".pre").map(&:chomp)
-			f2 = IO.readlines(workingdir + "/" + naming + ".post").map(&:chomp)
+			f1 = IO.readlines(Variables.workingdir + "/" + naming + ".pre").map(&:chomp)
+			f2 = IO.readlines(Variables.workingdir + "/" + naming + ".post").map(&:chomp)
 		File.open(Variables.workingdir + "/" + naming + ".out", "w"){ |f| f.write((f2 - f1).join("\n")) }
 		end
 
@@ -340,7 +340,7 @@ elsif os_decided == "windows"
 		Variables.name_files.each do |naming|
 			f1 = IO.readlines(Variables.workingdir + "/" + naming + ".pre").map(&:chomp)
 			f2 = IO.readlines(Variables.workingdir + "/" + naming + ".post").map(&:chomp)
-		File.open(workingdir + "/" + naming + ".out", "w"){ |f| f.write((f2 - f1).join("\n")) }
+		File.open(Variables.workingdir + "/" + naming + ".out", "w"){ |f| f.write((f2 - f1).join("\n")) }
 		end
 		puts Messages.post_analysis
 	end
