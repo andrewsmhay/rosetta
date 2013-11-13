e2f3ec84f511984a7cb2a114870bfa9171e61a47
Andrew Hay <andrewsmhay@gmail.com>
2013-11-12 17:58:16 -0800

    crazy test 2

--- lib/exclusions.rb
+++ lib/exclusions.rb
@@ -1,7 +1,8 @@
 class Exclusions
 	class << self
 		def fs_paths
-			"(! ((path.start_with? ".") || (path.start_with? "/dev/") || (path.start_with? "/proc/") || (path.start_with? "/sys/") || (path.start_with? "/root/") || (path.start_with? "/usr/share/doc/") || (path.start_with? "/var/lib/yum") || (path.start_with? "/home")))"
+			#(! ((path.start_with? ".") || (path.start_with? "/dev/") || (path.start_with? "/proc/") || (path.start_with? "/sys/") || (path.start_with? "/root/") || (path.start_with? "/usr/share/doc/") || (path.start_with? "/var/lib/yum") || (path.start_with? "/home")))
+			((path.start_with? ".") || (path.start_with? "/dev/") || (path.start_with? "/proc/") || (path.start_with? "/sys/") || (path.start_with? "/root/") || (path.start_with? "/usr/share/doc/") || (path.start_with? "/var/lib/yum") || (path.start_with? "/home"))
 		end
 	end
 end
