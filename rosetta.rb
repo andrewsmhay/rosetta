e59f689b29ba93cbfe10ac623957153acbba8378
Andrew Hay <andrewsmhay@gmail.com>
2013-11-12 18:53:22 -0800

    fs_fxt

--- rosetta.rb
+++ rosetta.rb
@@ -15,7 +15,7 @@ os_decided = os_select.os.to_s
 #workingdir = '.'
 #apt_file_inst = "/usr/bin/apt-get install apt-file -y > /dev/null && /usr/bin/apt-file update > /dev/null"
 #inputter = []
-#fs_ext = ['pre', 'post', 'out']
+#Variables.fs_ext = ['pre', 'post', 'out']
 #opt_sel = ['pre', 'post', 'final']
 #opt_sel_err = "[-] Usage: ./rosetta.rb <package_name> <pre> | <post|final>"
 #fs_footprint_fin = "Finished footprinting root filesystem. Results stored in " + fs_find_file
@@ -78,8 +78,8 @@ if os_decided == "nix" && File.exist?("/usr/bin/apt-get")
    				inputter << path + "\n"
    			end
  		end
-		File.open(Messages.fs_find_file+fs_ext[0], "w"){ |f| f.write(inputter)}
-		puts Messages.fs_footprint_fin+fs_ext[0]+"."
+		File.open(Messages.fs_find_file+Variables.fs_ext[0], "w"){ |f| f.write(inputter)}
+		puts Messages.fs_footprint_fin+Variables.fs_ext[0]+"."
 
 		# Package contents
 		puts ""
@@ -90,28 +90,28 @@ if os_decided == "nix" && File.exist?("/usr/bin/apt-get")
 		# Network services
 		puts ""
 		puts Messages.net_stat_txt
-		system(Cmd.net_stat+Messages.output_file_net_stat+fs_ext[0])
-		puts Messages.net_stat_txt_fin+fs_ext[0]+"."
+		system(Cmd.net_stat+Messages.output_file_net_stat+Variables.fs_ext[0])
+		puts Messages.net_stat_txt_fin+Variables.fs_ext[0]+"."
 
 		# Group information
 		puts ""
 		puts Messages.group_list_txt_fp
 		Etc.group {|g| Variables.group_list_txt_fin << g.name + ": " + g.mem.join(', ') + "\n"}
-		File.open(Messages.output_file_group+fs_ext[0], "w"){ |f| f.write(Variables.group_list_txt_fin)}
-		puts Messages.group_list_txt+fs_ext[0]+"."
+		File.open(Messages.output_file_group+Variables.fs_ext[0], "w"){ |f| f.write(Variables.group_list_txt_fin)}
+		puts Messages.group_list_txt+Variables.fs_ext[0]+"."
 
 		# User information
 		puts ""
 		puts Messages.user_list_txt_fp
 		Etc.passwd {|u| Variables.user_list_txt_fin << u.name + " = " + u.gecos + "\n"}
-		File.open(Messages.output_file_user+fs_ext[0], "w"){ |f| f.write(Variables.user_list_txt_fin)}
-		puts Messages.user_list_txt+fs_ext[0]+"."
+		File.open(Messages.output_file_user+Variables.fs_ext[0], "w"){ |f| f.write(Variables.user_list_txt_fin)}
+		puts Messages.user_list_txt+Variables.fs_ext[0]+"."
 
 		# CHKCONFIG Information
 		puts ""
 		puts Messages.chk_config_txt
-		system(Cmd.chk_config+Messages.output_file_chk_config+fs_ext[0])
-		puts Messages.chk_config_txt_fin+fs_ext[0]+"."
+		system(Cmd.chk_config+Messages.output_file_chk_config+Variables.fs_ext[0])
+		puts Messages.chk_config_txt_fin+Variables.fs_ext[0]+"."
 
 		# Startup binaries
 		puts ""
@@ -120,7 +120,7 @@ if os_decided == "nix" && File.exist?("/usr/bin/apt-get")
         Variables.rc_list_txt_fin << pathrc + "\n"
 			end
 		end
-		File.open(Messages.output_file_rc+fs_ext[0], "w"){ |f| f.write(Variables.rc_list_txt_fin)}
+		File.open(Messages.output_file_rc+Variables.fs_ext[0], "w"){ |f| f.write(Variables.rc_list_txt_fin)}
 
 	elsif ARGV[1] == Variables.opt_sel[1]
 		puts Messages.post_fs_footprint
@@ -132,35 +132,35 @@ if os_decided == "nix" && File.exist?("/usr/bin/apt-get")
    				inputter << path + "\n"
       		end
  		end
-		File.open(fs_find_file+fs_ext[1], "w"){ |f| f.write(inputter)}
-		puts Messages.fs_footprint_fin+fs_ext[1]+"."
+		File.open(fs_find_file+Variables.fs_ext[1], "w"){ |f| f.write(inputter)}
+		puts Messages.fs_footprint_fin+Variables.fs_ext[1]+"."
 
 		# Package contents not required for post-install footprinting
 		# Network services
 		puts ""
 		puts Messages.net_stat_txt
-		system(Cmd.net_stat+Messages.output_file_net_stat+fs_ext[1])
-		puts Messages.net_stat_txt_fin+fs_ext[1]+"."
+		system(Cmd.net_stat+Messages.output_file_net_stat+Variables.fs_ext[1])
+		puts Messages.net_stat_txt_fin+Variables.fs_ext[1]+"."
 
 		# Group information
 		puts ""
 		puts Messages.group_list_txt_fp
 		Etc.group {|g| Variables.group_list_txt_fin << g.name + ": " + g.mem.join(', ') + "\n"}
-		File.open(Messages.output_file_group+fs_ext[1], "w"){ |f| f.write(Variables.group_list_txt_fin)}
-		puts Messages.group_list_txt+fs_ext[1]+"."
+		File.open(Messages.output_file_group+Variables.fs_ext[1], "w"){ |f| f.write(Variables.group_list_txt_fin)}
+		puts Messages.group_list_txt+Variables.fs_ext[1]+"."
 
 		# User information
 		puts ""
 		puts Messages.user_list_txt_fp
 		Etc.passwd {|u| Variables.user_list_txt_fin << u.name + " = " + u.gecos + "\n"}
-		File.open(Messages.output_file_user+fs_ext[1], "w"){ |f| f.write(Variables.user_list_txt_fin)}
-		puts Messages.user_list_txt+fs_ext[1]+"."
+		File.open(Messages.output_file_user+Variables.fs_ext[1], "w"){ |f| f.write(Variables.user_list_txt_fin)}
+		puts Messages.user_list_txt+Variables.fs_ext[1]+"."
 
 		# CHKCONFIG Information
 		puts ""
 		puts Messages.chk_config_txt
-		system(Cmd.chk_config+Messages.output_file_chk_config+fs_ext[1])
-		puts Messages.chk_config_txt_fin+fs_ext[1]+"."
+		system(Cmd.chk_config+Messages.output_file_chk_config+Variables.fs_ext[1])
+		puts Messages.chk_config_txt_fin+Variables.fs_ext[1]+"."
 
 		# Startup binaries
 		puts ""
@@ -168,7 +168,7 @@ if os_decided == "nix" && File.exist?("/usr/bin/apt-get")
 			Find.find(rc_list) do |pathrc| Variables.rc_list_txt_fin << pathrc + "\n"
 			end
 		end
-		File.open(Messages.output_file_rc+fs_ext[1], "w"){ |f| f.write(Variables.rc_list_txt_fin)}
+		File.open(Messages.output_file_rc+Variables.fs_ext[1], "w"){ |f| f.write(Variables.rc_list_txt_fin)}
 
 	else ARGV[1] == Variables.opt_sel[2]
 		puts Messages.post_a_compare
@@ -192,7 +192,7 @@ if os_decided == "nix" && File.exist?("/usr/bin/apt-get")
 			elsif filetype =~ /\.json/
 				filetype_ary << filetype
 			end
-		File.open(Messages.output_filetype_ary+fs_ext[1], "w"){ |f| f.write((filetype_ary).join("\n"))}
+		File.open(Messages.output_filetype_ary+Variables.fs_ext[1], "w"){ |f| f.write((filetype_ary).join("\n"))}
 		end
 		puts Messages.post_analysis
 	end
@@ -210,36 +210,36 @@ elsif os_decided == "nix" && File.exist?(Variables.package_rh)
 		Cmd.fs_open
 		Cmd.exclude_and_write
 		Cmd.close_file
-		#f = File.open(Messages.fs_find_file+fs_ext[0], "w")
+		#f = File.open(Messages.fs_find_file+Variables.fs_ext[0], "w")
 		#Find.find('/'){|path| f.write(path + "\n") != ((path.start_with? ".") || (path.start_with? "/dev/") || (path.start_with? "/proc/") || (path.start_with? "/sys/") || (path.start_with? "/root/") || (path.start_with? "/usr/share/doc/") || (path.start_with? "/var/lib/yum") || (path.start_with? "/home"))}
 		#f.close()
-		puts Messages.fs_footprint_fin+fs_ext[0]+"."
+		puts Messages.fs_footprint_fin+Variables.fs_ext[0]+"."
 		# Network services
 		puts ""
 		puts Messages.net_stat_txt
-		system(Cmd.net_stat+Messages.output_file_net_stat+fs_ext[0])
-		puts Messages.net_stat_txt_fin+fs_ext[0]+"."
+		system(Cmd.net_stat+Messages.output_file_net_stat+Variables.fs_ext[0])
+		puts Messages.net_stat_txt_fin+Variables.fs_ext[0]+"."
 		# Group information
 		puts ""
 		puts Messages.group_list_txt_fp
 		Etc.group {|g| Variables.group_list_txt_fin << g.name + ": " + g.mem.join(', ') + "\n"}
-		File.open(Messages.output_file_group+fs_ext[0], "w"){ |f| f.write(Variables.group_list_txt_fin)}
-		puts Messages.group_list_txt+fs_ext[0]+"."
+		File.open(Messages.output_file_group+Variables.fs_ext[0], "w"){ |f| f.write(Variables.group_list_txt_fin)}
+		puts Messages.group_list_txt+Variables.fs_ext[0]+"."
 		# User information
 		puts ""
 		puts Messages.user_list_txt_fp
 		Etc.passwd {|u| Variables.user_list_txt_fin << u.name + " = " + u.gecos + "\n"}
-		File.open(Messages.output_file_user+fs_ext[0], "w"){ |f| f.write(Variables.user_list_txt_fin)}
-		puts Messages.user_list_txt+fs_ext[0]+"."
+		File.open(Messages.output_file_user+Variables.fs_ext[0], "w"){ |f| f.write(Variables.user_list_txt_fin)}
+		puts Messages.user_list_txt+Variables.fs_ext[0]+"."
 		# CHKCONFIG Information
 		puts ""
 		puts Messages.chk_config_txt
-		system(Cmd.chk_config+Messages.output_file_chk_config+fs_ext[0])
-		puts Messages.chk_config_txt_fin+fs_ext[0]+"."
+		system(Cmd.chk_config+Messages.output_file_chk_config+Variables.fs_ext[0])
+		puts Messages.chk_config_txt_fin+Variables.fs_ext[0]+"."
 		# Startup binaries
 		puts ""
 		Dir.glob("/etc/rc?.d").each { |rc_list| Find.find(rc_list) { |pathrc| Variables.rc_list_txt_fin << pathrc + "\n"}}
-		File.open(Messages.output_file_rc+fs_ext[0], "w"){ |f| f.write(Variables.rc_list_txt_fin)}
+		File.open(Messages.output_file_rc+Variables.fs_ext[0], "w"){ |f| f.write(Variables.rc_list_txt_fin)}
 	
 	elsif ARGV[1] == Variables.opt_sel[1]
 		puts Messages.post_fs_footprint
@@ -251,37 +251,37 @@ elsif os_decided == "nix" && File.exist?(Variables.package_rh)
    				inputter << path + "\n"
    			end
  		end
-		File.open(Messages.fs_find_file+fs_ext[1], "w"){ |f| f.write(inputter)}
-		puts Messages.fs_footprint_fin+fs_ext[1]+"."
+		File.open(Messages.fs_find_file+Variables.fs_ext[1], "w"){ |f| f.write(inputter)}
+		puts Messages.fs_footprint_fin+Variables.fs_ext[1]+"."
 		# Network services
 		puts ""
 		puts Messages.net_stat_txt
-		system(Cmd.net_stat+Messages.output_file_net_stat+fs_ext[1])
-		puts Messages.net_stat_txt_fin+fs_ext[1]+"."
+		system(Cmd.net_stat+Messages.output_file_net_stat+Variables.fs_ext[1])
+		puts Messages.net_stat_txt_fin+Variables.fs_ext[1]+"."
 		# Group information
 		puts ""
 		puts Messages.group_list_txt_fp
 		Etc.group {|g| Variables.group_list_txt_fin << g.name + ": " + g.mem.join(', ') + "\n"}
-		File.open(Messages.output_file_group+fs_ext[1], "w"){ |f| f.write(Variables.group_list_txt_fin)}
-		puts Messages.group_list_txt+fs_ext[1]+"."
+		File.open(Messages.output_file_group+Variables.fs_ext[1], "w"){ |f| f.write(Variables.group_list_txt_fin)}
+		puts Messages.group_list_txt+Variables.fs_ext[1]+"."
 		# User information
 		puts ""
 		puts Messages.user_list_txt_fp
 		Etc.passwd {|u| Variables.user_list_txt_fin << u.name + " = " + u.gecos + "\n"}
-		File.open(Messages.output_file_user+fs_ext[1], "w"){ |f| f.write(Variables.user_list_txt_fin)}
-		puts Messages.user_list_txt+fs_ext[1]+"."
+		File.open(Messages.output_file_user+Variables.fs_ext[1], "w"){ |f| f.write(Variables.user_list_txt_fin)}
+		puts Messages.user_list_txt+Variables.fs_ext[1]+"."
 		# CHKCONFIG Information
 		puts ""
 		puts Messages.chk_config_txt
-		system(Cmd.chk_config+Messages.output_file_chk_config+fs_ext[1])
-		puts Messages.chk_config_txt_fin+fs_ext[1]+"."
+		system(Cmd.chk_config+Messages.output_file_chk_config+Variables.fs_ext[1])
+		puts Messages.chk_config_txt_fin+Variables.fs_ext[1]+"."
 		# Startup binaries
 		puts ""
 		Dir.glob("/etc/rc?.d").each do |rc_list|
 			Find.find(rc_list) do |pathrc| Variables.rc_list_txt_fin << pathrc + "\n"
 			end
 		end
-		File.open(Messages.output_file_rc+fs_ext[1], "w"){ |f| f.write(Variables.rc_list_txt_fin)}
+		File.open(Messages.output_file_rc+Variables.fs_ext[1], "w"){ |f| f.write(Variables.rc_list_txt_fin)}
 
 	else ARGV[1] == Variables.opt_sel[2]
 		puts Messages.post_a_compare
@@ -305,7 +305,7 @@ elsif os_decided == "nix" && File.exist?(Variables.package_rh)
 			elsif filetype =~ /\.json/
 				filetype_ary << filetype
 			end
-		File.open(Messages.output_filetype_ary+fs_ext[1], "w"){ |f| f.write((filetype_ary).join("\n"))}
+		File.open(Messages.output_filetype_ary+Variables.fs_ext[1], "w"){ |f| f.write((filetype_ary).join("\n"))}
 		end
 		puts Messages.post_analysis
 	end
@@ -324,32 +324,32 @@ elsif os_decided == "windows"
 		Find.find('/') do |path|
    			inputter << path + "\n"
  		end
-		File.open(Messages.fs_find_file+fs_ext[0], "w"){ |f| f.write(inputter)}
-		puts Messages.fs_footprint_fin+fs_ext[0]+"."
+		File.open(Messages.fs_find_file+Variables.fs_ext[0], "w"){ |f| f.write(inputter)}
+		puts Messages.fs_footprint_fin+Variables.fs_ext[0]+"."
 
 		# Network services
 		puts ""
 		puts Messages.net_stat_txt
-		system(Cmd.net_stat_win+Messages.output_file_net_stat+fs_ext[0])
-		puts Messages.net_stat_txt_fin+fs_ext[0]+"."
+		system(Cmd.net_stat_win+Messages.output_file_net_stat+Variables.fs_ext[0])
+		puts Messages.net_stat_txt_fin+Variables.fs_ext[0]+"."
 
 		# Group information
 		puts ""
 		puts Messages.group_list_txt_fp
 		system(wmic GROUP > group.pre)
-		puts Messages.group_list_txt+fs_ext[0]+"."
+		puts Messages.group_list_txt+Variables.fs_ext[0]+"."
 
  		# User information
 		puts ""
 		puts Messages.user_list_txt_fp
 		system(wmic USERACCOUNT LIST FULL > user.pre)
-		puts Messages.user_list_txt+fs_ext[0]+"."
+		puts Messages.user_list_txt+Variables.fs_ext[0]+"."
 
 		# CHKCONFIG Information
 		puts ""
 		puts Messages.chk_config_txt
 		system(wmic SERVICE LIST FULL > services.pre)
-		puts Messages.chk_config_txt_fin+fs_ext[0]+"."
+		puts Messages.chk_config_txt_fin+Variables.fs_ext[0]+"."
 
 =begin		
 		# Windows Registry
@@ -367,32 +367,32 @@ elsif os_decided == "windows"
 		Find.find('/') do |path|
    			inputter << path + "\n"
    		end
-		File.open(Messages.fs_find_file+fs_ext[1], "w"){ |f| f.write(inputter)}
-		puts Messages.fs_footprint_fin+fs_ext[1]+"."
+		File.open(Messages.fs_find_file+Variables.fs_ext[1], "w"){ |f| f.write(inputter)}
+		puts Messages.fs_footprint_fin+Variables.fs_ext[1]+"."
 
 		# Network services
 		puts ""
 		puts Messages.net_stat_txt
-		system(Cmd.net_stat_win+Messages.output_file_net_stat+fs_ext[1])
-		puts Messages.net_stat_txt_fin+fs_ext[1]+"."
+		system(Cmd.net_stat_win+Messages.output_file_net_stat+Variables.fs_ext[1])
+		puts Messages.net_stat_txt_fin+Variables.fs_ext[1]+"."
 
 		# Group information
 		puts ""
 		puts Messages.group_list_txt_fp
 		system(wmic GROUP > group.post)
-		puts Messages.group_list_txt+fs_ext[1]+"."
+		puts Messages.group_list_txt+Variables.fs_ext[1]+"."
 
 		# User information
 		puts ""
 		puts Messages.user_list_txt_fp
 		system(wmic USERACCOUNT LIST FULL > user.pre)
-		puts Messages.user_list_txt+fs_ext[1]+"."
+		puts Messages.user_list_txt+Variables.fs_ext[1]+"."
 
 		# CHKCONFIG Information
 		puts ""
 		puts Messages.chk_config_txt
-		system(Cmd.chk_config+Messages.output_file_chk_config+fs_ext[1])
-		puts Messages.chk_config_txt_fin+fs_ext[1]+"."
+		system(Cmd.chk_config+Messages.output_file_chk_config+Variables.fs_ext[1])
+		puts Messages.chk_config_txt_fin+Variables.fs_ext[1]+"."
 		
 		#Windows Registry
 
