56f5490a6dd09bc5a37f435451c15235c7512418
Skyler Hawthorne <shawthorne@cloudpassage.com>
2014-01-13 16:39:43 -0800

    Whoops. Removed test files.

--- 1.rb
+++
@@ -1,7 +0,0 @@
-puts Messages.post_a_compare
-Variables.name_files.each do |naming|
-	f1 = IO.readlines(Variables.workingdir + "/" + naming + ".pre").map(&:chomp)
-	f2 = IO.readlines(Variables.workingdir + "/" + naming + ".post").map(&:chomp)
-	File.open(Variables.workingdir + "/" + naming + ".out", "w"){ |f| f.write((f2 - f1).join("\n")) }
-end
-puts Messages.post_analysis
