44db1d97a1052e496315027e5962d4b63832cbd9
Skyler Hawthorne <shawthorne@cloudpassage.com>
2014-01-15 10:19:39 -0800

    Moved Windows footprinting into its own function.

--- rosetta.rb
+++ rosetta.rb
@@ -63,7 +63,52 @@ def footprint_nix(fs_ext = "pre", os="")
 	puts Messages.services_finished+fs_ext+"."
 end
 
+def footprint_win(fs_ext = "pre", os="")
+
+	# Filesystem footprinting
+	puts ""
+	puts Messages.fs_footprint
+	f = File.open(Messages.fs_find_file+fs_ext, "w")
+	Find.find('/'){|path| f.write(path + "\r\n") unless (path.start_with? ".") || (path.start_with? "/$Recycle.Bin")}
+	f.close()
+	puts Messages.fs_footprint_fin+fs_ext+"."
+
+	# Network services
+	puts ""
+	puts Messages.net_stat_txt
+	system(Cmd.net_stat_win+Messages.output_file_net_stat+fs_ext)
+	puts Messages.net_stat_txt_fin+fs_ext+"."
+
+	# Group information
+	puts ""
+	puts Messages.group_list_txt_fp
+	system(Cmd.wmic_grp + Messages.output_file_group + fs_ext)
+	puts Messages.group_list_txt+fs_ext+"."
+
+	# User information
+	puts ""
+	puts Messages.user_list_txt_fp
+	system(Cmd.wmic_usr + Messages.output_file_user + fs_ext)
+	puts Messages.user_list_txt+fs_ext+"."
+
+	# Services Information
+	puts ""
+	puts Messages.services_txt
+	system(Cmd.wmic_srv + Messages.output_file_services + fs_ext)
+	puts Messages.services_finished+fs_ext+"."
+	
+	#Windows Registry
+	puts ""
+	puts Messages.reg_fp
+	Variables.reg_roots.each_with_index do |root, i|
+		puts Messages.reg_fp_single(root, i, Variables.reg_roots.length)
+		system(Cmd.win_reg + root + " /s >> registry." + fs_ext)
+	end
+	puts Messages.reg_fp_done + fs_ext
+end
+
 def final_compare_nix
+	# All single line comparisons, so just do a line-by-line set difference.
 	puts Messages.post_a_compare
 	Variables.name_files.each do |naming|
 		f1 = IO.readlines(Variables.workingdir + "/" + naming + ".pre").map(&:chomp)
@@ -87,6 +132,25 @@ def final_compare_nix
 		end
 		File.open(Messages.output_filetype_ary+Variables.fs_ext[2], "w"){ |f| f.write((@filetype_ary).join("\n"))}
 	end
+	puts Messages.post_analysis
+end
+
+def final_compare_win
+	puts Messages.post_a_compare
+	Variables.name_files.each do |naming|
+		f1 = IO.readlines(Variables.workingdir + "/" + naming + ".pre").map(&:chomp)
+		f2 = IO.readlines(Variables.workingdir + "/" + naming + ".post").map(&:chomp)
+		File.open(Variables.workingdir + "/" + naming + ".out", "w"){ |f| f.write((f2 - f1).join("\r\n")) }
+	end
+
+	# Registry has a different operation, so it's not part of the loop above
+	reg_data_pre = File.open(Messages.output_file_reg + Variables.fs_ext[0]).read.split(/\n(?=HKEY)/).map(&:strip)
+	reg_data_post = File.open(Messages.output_file_reg + Variables.fs_ext[1]).read.split(/\n(?=HKEY)/).map(&:strip)
+	
+	File.open(Variables.workingdir + "/" + Messages.output_file_reg + Variables.fs_ext[2], "w") do |f|
+		f.write((reg_data_post - reg_data_pre).join("\r\n"))
+	end
+
 	puts Messages.post_analysis
 end
 
@@ -161,108 +225,19 @@ elsif @os_decided == "windows"
 	if ARGV[1] == Variables.opt_sel[0]
 		puts Messages.ms
 
-		# Filesystem footprinting
-		puts ""
-		puts Messages.fs_footprint
-		f = File.open(Messages.fs_find_file+Variables.fs_ext[0], "w")
-		Find.find('/'){|path| f.write(path + "\n") != ((path.start_with? ".") || (path.start_with? "/dev/") || (path.start_with? "/proc/") || (path.start_with? "/sys/") || (path.start_with? "/root/") || (path.start_with? "/usr/share/doc/") || (path.start_with? "/var/lib/yum") || (path.start_with? "/home"))}
-		f.close()
-		puts Messages.file_footprint_done
+		fs_ext = Variables.fs_ext[0]
 
-		# Network services
-		puts ""
-		puts Messages.net_stat_txt
-		system(Cmd.net_stat_win+Messages.output_file_net_stat+Variables.fs_ext[0])
-		puts Messages.net_stat_txt_fin+Variables.fs_ext[0]+"."
-
-		# Group information
-		puts ""
-		puts Messages.group_list_txt_fp
-		system(Cmd.wmic_grp + Messages.output_file_group + Variables.fs_ext[0])
-		puts Messages.group_list_txt+Variables.fs_ext[0]+"."
-
- 		# User information
-		puts ""
-		puts Messages.user_list_txt_fp
-		system(Cmd.wmic_usr + Messages.output_file_user + Variables.fs_ext[0])
-		puts Messages.user_list_txt+Variables.fs_ext[0]+"."
-
-		# Services Information
-		puts ""
-		puts Messages.services_txt
-		system(Cmd.wmic_srv + Messages.output_file_services + Variables.fs_ext[0])
-		puts Messages.services_finished+Variables.fs_ext[0]+"."
-	
-		# Windows Registry
-		puts ""
-		puts Messages.reg_fp
-		Variables.reg_roots.each_with_index do |root, i|
-			puts Messages.reg_fp_single(root, i, Variables.reg_roots.length)
-			system(Cmd.win_reg + root + " /s >> registry." + Variables.fs_ext[0])
-		end
-		puts Messages.reg_fp_done + Variables.fs_ext[0]
+		footprint_win(fs_ext, "windows")
 
 	elsif ARGV[1] == Variables.opt_sel[1]
 		puts Messages.post_fs_footprint
 
-		# Filesystem footprinting
-		puts ""
-		puts Messages.fs_footprint
-		f = File.open(Messages.fs_find_file+Variables.fs_ext[1], "w")
-		Find.find('/'){|path| f.write(path + "\n") != ((path.start_with? ".") || (path.start_with? "/dev/") || (path.start_with? "/proc/") || (path.start_with? "/sys/") || (path.start_with? "/root/") || (path.start_with? "/usr/share/doc/") || (path.start_with? "/var/lib/yum") || (path.start_with? "/home"))}
-		f.close()
-		puts Messages.fs_footprint_fin+Variables.fs_ext[1]+"."
+		fs_ext = Variables.fs_ext[1]
 
-		# Network services
-		puts ""
-		puts Messages.net_stat_txt
-		system(Cmd.net_stat_win+Messages.output_file_net_stat+Variables.fs_ext[1])
-		puts Messages.net_stat_txt_fin+Variables.fs_ext[1]+"."
-
-		# Group information
-		puts ""
-		puts Messages.group_list_txt_fp
-		system(Cmd.wmic_grp + Messages.output_file_group + Variables.fs_ext[1])
-		puts Messages.group_list_txt+Variables.fs_ext[1]+"."
-
-		# User information
-		puts ""
-		puts Messages.user_list_txt_fp
-		system(Cmd.wmic_usr + Messages.output_file_user + Variables.fs_ext[1])
-		puts Messages.user_list_txt+Variables.fs_ext[1]+"."
-
-		# Services Information
-		puts ""
-		puts Messages.services_txt
-		system(Cmd.wmic_srv + Messages.output_file_services + Variables.fs_ext[1])
-		puts Messages.services_finished+Variables.fs_ext[1]+"."
-		
-		#Windows Registry
-		puts ""
-		puts Messages.reg_fp
-		Variables.reg_roots.each_with_index do |root, i|
-			puts Messages.reg_fp_single(root, i, Variables.reg_roots.length)
-			system(Cmd.win_reg + root + " /s >> registry." + Variables.fs_ext[1])
-		end
-		puts Messages.reg_fp_done + Variables.fs_ext[1]
+		footprint_win(fs_ext, "windows")
 
 	else ARGV[1] == Variables.opt_sel[2]
-		puts Messages.post_a_compare
-		Variables.name_files.each do |naming|
-			f1 = IO.readlines(Variables.workingdir + "/" + naming + ".pre").map(&:chomp)
-			f2 = IO.readlines(Variables.workingdir + "/" + naming + ".post").map(&:chomp)
-			File.open(Variables.workingdir + "/" + naming + ".out", "w"){ |f| f.write((f2 - f1).join("\r\n")) }
-		end
-
-		# Registry has a different operation, so it's not part of the loop above
-		reg_data_pre = File.open(Messages.output_file_reg + Variables.fs_ext[0]).read.split(/\n(?=HKEY)/).map(&:strip)
-		reg_data_post = File.open(Messages.output_file_reg + Variables.fs_ext[1]).read.split(/\n(?=HKEY)/).map(&:strip)
-		
-		File.open(Variables.workingdir + "/" + Messages.output_file_reg + Variables.fs_ext[2], "w") do |f|
-			f.write((reg_data_post - reg_data_pre).join("\r\n"))
-		end
-
-		puts Messages.post_analysis
+		final_compare_win
 	end
 	
 else
